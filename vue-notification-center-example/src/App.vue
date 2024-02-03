df7327f6bc3b6d65a5773496a39be46a0aa9883d
Pawan Jain <jainpawan211199@gmail.com>
2024-02-03 23:39:42 +0530

    fix: update App.vue with text changes

--- vue-notification-center-example/src/App.vue
+++ vue-notification-center-example/src/App.vue
@@ -3,20 +3,17 @@ import NotificationCenterWrapper from './components/NotificationCenterWrapper.vu
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header> -->
    <main>
-    Change <b>SUBSCRIBER_ID</b> and <b>APPLICATION_IDENTIFIER</b> values in App.js to remove loader
+    Change <span class="bold">subscriberId</span> and <span class="bold">applicationIdentifier</span> values in <code>src/components/NotificationCenterWrapper.vue</code> to your own values.
     <NotificationCenterWrapper />
-   </main>
-
-   
+   </main>   
 </template>
 
 <style scoped>
+
+.bold {
+  font-size: "18px";
+  font-weight: bold;
+}
+
 </style>
