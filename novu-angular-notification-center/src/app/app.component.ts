51c0e463cc5a2a9b9526f864a69f492417c57d1c
Pawan Jain <jainpawan211199@gmail.com>
2024-02-10 13:19:02 +0530

    fix: add angulr props

--- novu-angular-notification-center/src/app/app.component.ts
+++ novu-angular-notification-center/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-
-const SUBSCRIBER_ID = '651ece9ae1b82d64b388943a';
-const APPLICATION_ID = 'Kzaqr_H9HtvD';
+import { styles } from './styles';
+const SUBSCRIBER_ID = 'SUBSCRIBER_ID';
+const APPLICATION_ID = 'APPLICAITON_IDENTIFIER';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +11,82 @@ const APPLICATION_ID = 'Kzaqr_H9HtvD';
 export class AppComponent {
   title = 'novu-angular-notification-center';
 
+  // required props for notification center
   subscriberId = SUBSCRIBER_ID;
   applicationIdentifier = APPLICATION_ID;
+
+  // optional props for notification center
   backendUrl = 'https://api.novu.co';
   socketUrl = 'https://ws.novu.co';
+  subscriberHash = 'SUBSCRIBER_HASH';
+  showUserPreferences = true;
+  allowedNotificationActions = true;
+  popoverConfig = { offset: 10 };
+  i18n = {
+    lang: 'en',
+
+    translations: {
+      poweredBy: 'Powered By',
+    },
+  };
+
+  stores = [
+    {
+      storeId: 'default_store',
+    },
+    /** storeId and feedIdentifier must be identical and should be copied from In-App editor feed */
+    {
+      storeId: 'product-updates',
+      query: { feedIdentifier: 'product-updates' },
+    },
+    {
+      storeId: 'user-activity',
+      query: { feedIdentifier: 'user-activity' },
+    },
+  ];
+
+  tabs = [
+    { name: 'All Notifications', storeId: 'default_store' },
+    // name can be any custom name
+    { name: 'Product Updates', storeId: 'product-updates' },
+    { name: 'User Activity', storeId: 'user-activity' },
+  ];
+  styles = styles;
 
   sessionLoaded = (data: unknown) => {
     console.log('loaded -->', { data });
   };
-  popoverConfig = { offset: 10 };
+
+  notificationClicked = (message: any) => {
+    console.log('Notification clicked:', message);
+  };
+
+  unseenCountChanged = (count: number) => {
+    console.log('Unseen count changed', count);
+  };
+
+  actionClicked = (
+    workflowTriggerIdentifier: string,
+    actionButtonType: string,
+    message: any
+  ) => {
+    console.log(
+      'Action clicked - workflowTriggerIdentifier:',
+      workflowTriggerIdentifier
+    );
+    console.log('Action clicked - actionButtonType:', actionButtonType);
+    console.log('Action clicked - message:', message);
+  };
+
+  tabClicked = (data: any) => {
+    console.log('Tab clicked:', data);
+  };
+
+  preferenceFilter = (preferences: any) => {
+    if (preferences.template.tags.includes('don-not-show-tag')) {
+      return false;
+    } else {
+      return true;
+    }
+  };
 }
--- novu-angular-notification-center/src/app/styles.ts
+++ novu-angular-notification-center/src/app/styles.ts
@@ -1,30 +1,30 @@
-const primaryColor = "white";
-const secondaryColor = "#AFE1AF";
-const primaryTextColor = "#0C0404";
-const secondaryTextColor = "#494F55";
-const unreadBackGroundColor = "#869F9F";
+const primaryColor = 'white';
+const secondaryColor = '#AFE1AF';
+const primaryTextColor = '#0C0404';
+const secondaryTextColor = '#494F55';
+const unreadBackGroundColor = '#869F9F';
 const primaryButtonBackGroundColor = unreadBackGroundColor;
-const secondaryButtonBackGroundColor = "#C6DFCD";
-const dropdownBorderStyle = "2px solid #AFE1AF";
-const tabLabelAfterStyle = "#AFE1AF !important";
+const secondaryButtonBackGroundColor = '#C6DFCD';
+const dropdownBorderStyle = '2px solid #AFE1AF';
+const tabLabelAfterStyle = '#AFE1AF !important';
 
 export const styles = {
   bellButton: {
     root: {
-      marginTop: "20px",
+      marginTop: '20px',
       svg: {
         color: secondaryColor,
         fill: primaryColor,
-        minWidth: "20px",
-        minHeight: "20px",
+        minWidth: '20px',
+        minHeight: '20px',
       },
     },
     dot: {
       rect: {
         fill: primaryColor,
-        strokeWidth: "0",
-        width: "3px",
-        height: "3px",
+        strokeWidth: '0',
+        width: '3px',
+        height: '3px',
         x: 10,
         y: 2,
       },
@@ -41,20 +41,20 @@ export const styles = {
     },
     dropdown: {
       border: dropdownBorderStyle,
-      borderRadius: "10px",
+      borderRadius: '10px',
     },
   },
   header: {
     root: {
       backgroundColor: primaryColor,
-      "&:hover": { backgroundColor: primaryColor },
-      cursor: "pointer",
+      '&:hover': { backgroundColor: primaryColor },
+      cursor: 'pointer',
       color: primaryTextColor,
     },
     cog: { opacity: 1 },
     markAsRead: {
       color: primaryTextColor,
-      fontSize: "14px",
+      fontSize: '14px',
     },
     title: { color: primaryTextColor },
     backButton: {
@@ -74,17 +74,17 @@ export const styles = {
   accordion: {
     item: {
       backgroundColor: secondaryColor,
-      ":hover": {
+      ':hover': {
         backgroundColor: secondaryColor,
       },
     },
     content: {
       backgroundColor: secondaryColor,
-      borderBottomLeftRadius: "7px",
-      borderBottomRightRadius: "7px",
+      borderBottomLeftRadius: '7px',
+      borderBottomRightRadius: '7px',
     },
     control: {
-      ":hover": {
+      ':hover': {
         backgroundColor: secondaryColor,
       },
       color: primaryTextColor,
@@ -99,15 +99,15 @@ export const styles = {
   switch: {
     root: {},
     input: {
-      ":checked~label": {
+      ':checked~label': {
         background: primaryColor,
-        ".mantine-Switch-trackLabel": {
+        '.mantine-Switch-trackLabel': {
           backgroundColor: primaryColor,
         },
       },
-      ":not(checked)~label": {
+      ':not(checked)~label': {
         background: primaryTextColor,
-        ".mantine-Switch-trackLabel": {
+        '.mantine-Switch-trackLabel': {
           backgroundColor: primaryTextColor,
         },
       },
@@ -117,45 +117,45 @@ export const styles = {
   },
   notifications: {
     root: {
-      ".nc-notifications-list-item": {
+      '.nc-notifications-list-item': {
         backgroundColor: secondaryColor,
       },
     },
     listItem: {
       layout: {
-        borderRadius: "7px",
+        borderRadius: '7px',
         color: primaryTextColor,
-        ".mantine-Avatar-root": {
-          width: "20px",
-          height: "20px",
-          minWidth: "20px",
+        '.mantine-Avatar-root': {
+          width: '20px',
+          height: '20px',
+          minWidth: '20px',
         },
-        ".mantine-Avatar-image": {
-          width: "20px",
-          height: "20px",
-          minWidth: "20px",
+        '.mantine-Avatar-image': {
+          width: '20px',
+          height: '20px',
+          minWidth: '20px',
         },
-        "div:has(> .mantine-Avatar-root)": {
-          border: "none",
-          width: "20px",
-          height: "20px",
-          minWidth: "20px",
+        'div:has(> .mantine-Avatar-root)': {
+          border: 'none',
+          width: '20px',
+          height: '20px',
+          minWidth: '20px',
         },
       },
-      timestamp: { color: secondaryTextColor, fontWeight: "bold" },
+      timestamp: { color: secondaryTextColor, fontWeight: 'bold' },
       dotsButton: {
         path: {
           fill: primaryTextColor,
         },
       },
       unread: {
-        "::before": { background: unreadBackGroundColor },
+        '::before': { background: unreadBackGroundColor },
       },
       buttons: {
         primary: {
           background: primaryButtonBackGroundColor,
           color: primaryTextColor,
-          "&:hover": {
+          '&:hover': {
             background: primaryButtonBackGroundColor,
             color: secondaryTextColor,
           },
@@ -163,7 +163,7 @@ export const styles = {
         secondary: {
           background: secondaryButtonBackGroundColor,
           color: secondaryTextColor,
-          "&:hover": {
+          '&:hover': {
             background: secondaryButtonBackGroundColor,
             color: secondaryTextColor,
           },
@@ -172,14 +172,14 @@ export const styles = {
     },
   },
   actionsMenu: {
-    item: { "&:hover": { backgroundColor: secondaryColor } },
+    item: { '&:hover': { backgroundColor: secondaryColor } },
     dropdown: {
       backgroundColor: primaryColor,
     },
     arrow: {
       backgroundColor: primaryColor,
-      borderTop: "0",
-      borderLeft: "0",
+      borderTop: '0',
+      borderLeft: '0',
     },
   },
   preferences: {
@@ -197,9 +197,9 @@ export const styles = {
   tabs: {
     tabLabel: {
       color: primaryTextColor,
-      "::after": { background: tabLabelAfterStyle },
-      ":hover": { color: tabLabelAfterStyle },
-      "[data-active]": { color: tabLabelAfterStyle },
+      '::after': { background: tabLabelAfterStyle },
+      ':hover': { color: tabLabelAfterStyle },
+      '[data-active]': { color: tabLabelAfterStyle },
     },
     tabsList: { borderBottomColor: primaryColor },
   },
