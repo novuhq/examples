b1015fcda5003c4f2e7d831eec4be46a5234724a
unicodeveloper <prosperotemuyiwa@gmail.com>
2024-06-27 07:00:49 +0100

    Update to novu

--- novu-workflows/react/password-reset/app/api/novu/route.ts
+++ novu-workflows/react/password-reset/app/api/novu/route.ts
@@ -1,4 +1,4 @@
 import { serve } from "@novu/framework/next";
-import { client, renderPasswordResetEmailWorkflow } from "../../echo/client";
+import { client, renderPasswordResetEmailWorkflow } from "../../novu/client";
 
 export const { GET, POST, PUT, OPTIONS } = serve({ client, workflows: [renderPasswordResetEmailWorkflow] });
