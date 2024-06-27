6fbaabd8cee0fc28262e350ec445938cf177c9e6
unicodeveloper <prosperotemuyiwa@gmail.com>
2024-06-27 06:59:03 +0100

    Update echo to novu

--- novu-workflows/react/otp/app/api/novu/route.ts
+++ novu-workflows/react/otp/app/api/novu/route.ts
@@ -1,4 +1,4 @@
 import { serve } from "@novu/framework/next";
-import { client, verificationOTP } from "../../echo/client";
+import { client, verificationOTP } from "../../novu/client";
 
 export const { GET, POST, PUT, OPTIONS } = serve({ client, workflows: [verificationOTP] });
